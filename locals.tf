28a247f1717affd4ed0042c2e322fee706f1fe1d
Daniel Dalavurak <morsh110792@yahoo.com>
2023-02-07 11:50:22 +0300

    Fixed fmt in local.tf

--- locals.tf
+++ locals.tf
@@ -19,12 +19,12 @@ locals {
   ansible_template_remote = templatefile(
     "${path.module}/templates/ansible_inventory_template_remote.tpl",
     {
-      ip1         = module.morsh_instance_ya_1.internal_ip_address_morsh_bastion,
-      hostname1   = module.morsh_instance_ya_1.hostname_morsh_bastion,
-      ip2         = module.morsh_instance_ya_2.internal_ip_address_morsh_bastion,
-      hostname2   = module.morsh_instance_ya_2.hostname_morsh_bastion
-      ip3         = module.morsh_instance_ya_3.internal_ip_address_morsh_bastion,
-      hostname3   = module.morsh_instance_ya_3.hostname_morsh_bastion
+      ip1       = module.morsh_instance_ya_1.internal_ip_address_morsh_bastion,
+      hostname1 = module.morsh_instance_ya_1.hostname_morsh_bastion,
+      ip2       = module.morsh_instance_ya_2.internal_ip_address_morsh_bastion,
+      hostname2 = module.morsh_instance_ya_2.hostname_morsh_bastion
+      ip3       = module.morsh_instance_ya_3.internal_ip_address_morsh_bastion,
+      hostname3 = module.morsh_instance_ya_3.hostname_morsh_bastion
 
 
     }
